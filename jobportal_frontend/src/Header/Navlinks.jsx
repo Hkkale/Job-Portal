c91f6fa0a0b6d8e0d66f148bd94a9c2e8ca5ecbe
Hitendra Kale <hitendrakale000@gmail.com>
2025-06-10 16:57:10 +0530

    header routing compleated

---
+++ jobportal_frontend/src/Header/Navlinks.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router';
+
+const Navlinks = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname;
+
+  const isActive = (pathName) => {
+    return path === pathName ? "text-bright-sun-400" : "text-white";
+  };
+
+  return (
+    <div className='flex gap-5 items-center'>
+      <h2
+        onClick={() => navigate("/findJob")}
+        className={`${isActive("/findJob")} cursor-pointer`}
+      >
+        Find job
+      </h2>
+      <h2
+        onClick={() => navigate("/findTalent")}
+        className={`${isActive("/findTalent")} cursor-pointer`}
+      >
+        Find Talent
+      </h2>
+      <h2
+        onClick={() => navigate("/uploadJob")}
+        className={`${isActive("/uploadJob")} cursor-pointer`}
+      >
+        Upload Job
+      </h2>
+      <h2
+        onClick={() => navigate("/aboutus")}
+        className={`${isActive("/aboutus")} cursor-pointer`}
+      >
+        About Us
+      </h2>
+    </div>
+  );
+};
+
+export default Navlinks;
