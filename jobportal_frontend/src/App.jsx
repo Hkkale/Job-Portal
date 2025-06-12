941ace578f3757cd96708da18aa113d5650570e7
Hitendra Kale <hitendrakale000@gmail.com>
2025-06-12 13:39:03 +0530

    Added Marquee section in landi ng page

--- jobportal_frontend/src/App.jsx
+++ jobportal_frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import { createTheme, MantineProvider } from "@mantine/core";
-import { Slider } from "@mantine/core";
+
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Route,Routes } from "react-router";
 
--- jobportal_frontend/src/Pages/HomePage.jsx
+++ jobportal_frontend/src/Pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../Header/Header'
 import DreamJob from '../LandingPage/DreamJob'
+import Companies from '../LandingPage/Companies'
 
 const HomePage = () => {
   return (
-    <div className='min-h-[100vh] bg-mine-shaft-950 font-[poppins] w-screen overflow-y-auto'>
+    <div className='min-h-[100vh] bg-mine-shaft-950 font-[poppins] w-screen border-2 border-white box-border overflow-x-hidden'>
     <Header/>
     <DreamJob/>
+    <Companies/>
+    
 
     </div>
   )
