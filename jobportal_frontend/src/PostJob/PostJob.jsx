877a4dfb21a1279c87bd228df0f75162c9dc11e4
Hitendra Kale <hitendrakale000@gmail.com>
2025-06-18 14:08:05 +0530

    talent profile page is completed

---
+++ jobportal_frontend/src/PostJob/PostJob.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const PostJob = () => {
+  return (
+    <div className='w-4/5 mx-auto mt-10'>
+
+      <div className='text-2xl font-semibold '>Post a Job</div>
+
+      <div>
+
+
+
+
+
+
+
+      </div>
+
+     
+      
+    </div>
+  )
+}
+
+export default PostJob
