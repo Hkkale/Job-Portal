2aee03065994ae6387a24812d92794c348a52c32
Hitendra Kale <hitendrakale000@gmail.com>
2025-07-15 23:26:48 +0530

    working on reset password

--- jobportal_backend/src/main/java/com/jobify/jobportal_backend/Service/UserServiceImpl.java
+++ jobportal_backend/src/main/java/com/jobify/jobportal_backend/Service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jobify.jobportal_backend.Service;
 
 import com.jobify.jobportal_backend.DTOs.LoginDto;
+import com.jobify.jobportal_backend.DTOs.ResponseDto;
 import com.jobify.jobportal_backend.DTOs.UserDto;
 import com.jobify.jobportal_backend.Entity.OTP;
 import com.jobify.jobportal_backend.Entity.User;
@@ -13,10 +14,12 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -102,6 +105,30 @@ public class UserServiceImpl implements UserService {
 
 
         return true;
+    }
+
+    @Override
+    public ResponseDto changePassword(LoginDto loginDto) throws JobPortalException {
+        User user = userRepository.findByEmail(loginDto.getEmail()).orElseThrow(() -> new JobPortalException("USER_NOT_FOUND"));
+        user.setPassword(passwordEncoder.encode(loginDto.getPassword()));
+        userRepository.save(user);
+
+        return new ResponseDto("Password changed successfully.");
+    }
+
+
+    @Scheduled(fixedRate = 60000)
+    public void removeExpiredOTPs(){
+        LocalDateTime expiry=LocalDateTime.now().minusMinutes(5);
+        List<OTP> expiredOtps=otpRepository.findByCreationTimeBefore(expiry);
+
+        if(!expiredOtps.isEmpty()){
+            otpRepository.deleteAll(expiredOtps);
+            System.out.println("Removed "+expiredOtps.size()+" expired otps");
+        }
+
+
+
     }
 
 
---
+++ jobportal_frontend/src/SignUp/ResetPassword.jsx
@@ -0,0 +1,12 @@
+import { Modal } from '@mantine/core'
+import React from 'react'
+
+const ResetPassword = (props) => {
+  return (
+   <Modal opened={props.opened} onClose={props.close} title="Authentication">
+
+   </Modal>
+  )
+}
+
+export default ResetPassword
